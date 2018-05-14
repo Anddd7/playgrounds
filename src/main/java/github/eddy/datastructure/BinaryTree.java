1c32c67b9e9731519344096a8042bdfe5faeb834
and777 <liaoad_space@sina.com>
2018-05-14 18:51:56 +0800

    refactor

---
+++ src/main/java/github/eddy/datastructure/BinaryTree.java
@@ -0,0 +1,11 @@
+package github.eddy.datastructure;
+
+/**
+ * @author Anddd7
+ * 二叉树
+ *
+ * TODO
+ */
+public class BinaryTree {
+
+}
---
+++ src/main/java/github/eddy/datastructure/LimitedArrayQueue.java
@@ -0,0 +1,36 @@
+package github.eddy.datastructure;
+
+/**
+ * @author Anddd7
+ * 有限单向循环队列
+ */
+public class LimitedArrayQueue<E> {
+
+  private final int capacity;
+  private int size;
+  private Object[] elements;
+  private int pushIndex = -1;
+  private int pollIndex = -1;
+
+  public LimitedArrayQueue(int capacity) {
+    this.capacity = Integer.highestOneBit(capacity) << 1;
+    this.elements = new Object[this.capacity];
+  }
+
+  public void push(E e) {
+    if (size == capacity) {
+      return;
+    }
+    elements[pushIndex = (pushIndex + 1) & (capacity - 1)] = e;
+    size++;
+  }
+
+  public E poll() {
+    if (size == 0) {
+      return null;
+    }
+    E value = (E) elements[pollIndex = (pollIndex + 1) & (capacity - 1)];
+    size--;
+    return value;
+  }
+}
