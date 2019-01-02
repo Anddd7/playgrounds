dc2ee10a8552847e90c972e480ec2849cf859105
Andong LIAO <liaoad_space@sina.com>
2019-01-03 00:51:37 +0800

    write leetcode with kotlin

---
+++ leetcode-kotlin/src/main/kotlin/com/github/anddd7/algorithms/SortTool.kt
@@ -0,0 +1,8 @@
+package com.github.anddd7.algorithms
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+
+fun IntArray.sort(): IntArray {
+    val result = IntArray(size)
+    throw NotImplementedException()
+}
---
+++ leetcode-kotlin/src/main/kotlin/com/github/anddd7/algorithms/easy/TwoSum.kt
@@ -0,0 +1,17 @@
+package com.github.anddd7.algorithms.easy
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+
+/**
+ * 在给定数组中存在2个元素, 相加等于target
+ */
+class TwoSum {
+    /**
+     * - 排序, 从两端向中间靠拢查询: 取决于排序算法复杂度
+     * - 循环一次, 通过HashMap快速确定值: O(n) | O(n)
+     * - 双层循环: O(n2) | O(1)
+     */
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        throw NotImplementedException()
+    }
+}
