af0270ff70cffdea1ae0c0a7df9f45a85159d5a4
Anddd7 <liaoad_space@sina.com>
2020-04-29 15:12:38 +0800

    [leetcode-1095] https://leetcode-cn.com/problems/find-in-mountain-array/

--- leetcode-kotlin/src/main/kotlin/com/github/anddd7/leetcode/Solution.kt
+++ leetcode-kotlin/src/main/kotlin/com/github/anddd7/leetcode/Solution.kt
@@ -1,50 +1,44 @@
 package com.github.anddd7.leetcode
 
 object Solution {
-  /**
-   * Round 1
-   * 通过二维数组记录动态状态
-   * 0 A[0]
-   * 1 A[0]|A[1] : A[1]
-   * 2 A[0]|A[1]|A[2] : A[1]|A[2] : A[2]
-   * ...
-   * - temp[i][j] = A[j] | A[j+1] ... A[i-1] | A[i]
-   * - 记录所有结果到set
-   * - return set.size
-   * > 内存溢出, 当n非常大的时候, 二维数组占空间太多
-   *
-   * Round 2
-   * 只记录最近一次的结果, 一维数组即可
-   * A[0] ...
-   * A[0]=A[0]|A[1] : A[1] ...
-   * A[0]=A[0]|A[2] : A[1]=A[1]|A[2] : A[2] ...
-   * ...
-   * - A[j] = A[j]|A[i]
-   * > 超时, 假设:
-   * > 当某一个 A[j] & A[i] = A[i] 的时候, A[i]所有的1在A[j]的对应位置也是1
-   * > 因为我们使用一维数组, A[j]之前的元素都是是"包含(|)"A[j], 1只多不少
-   * > 因此后续的A[0]~A[j] | A[i]时, 值不会再变化, 可以直接跳过
-   * > e.g 11111  01111  00111  00101  |  00100
-   * > 可以直接跳过
-   *
-   * Round 3
-   * 增加一个跳过选项
-   *
-   */
-  fun subarrayBitwiseORs(A: IntArray): Int {
-    val result = mutableSetOf<Int>()
+  interface MountainArray {
+    fun get(index: Int): Int
+    fun length(): Int
+  }
 
-    for (i in A.indices) {
-      result.add(A[i])
+  fun findInMountainArray(target: Int, mountainArr: MountainArray): Int {
+    val top = findTop(mountainArr, 0, mountainArr.length() - 1)
+    val max = mountainArr.get(top)
+    if (max == target) return top
+    if (max < target) return -1
 
-      // why reversed: 越靠左, 里面包含的操作数越多, A[0]|A[1]...|A[i] = A[0]|A[1]...A[i-1]  |  A[i]
-      for (j in (0 until i).reversed()) {
-        if (A[j] and A[i] == A[i]) break
-        A[j] = A[j] or A[i]
-        result.add(A[j])
-      }
+    return findNumber(mountainArr, 0, top - 1, target, false)
+        ?: findNumber(mountainArr, top + 1, mountainArr.length() - 1, target, true)
+        ?: -1
+  }
+
+  private fun findNumber(mountainArr: MountainArray, start: Int, end: Int, target: Int, desc: Boolean): Int? {
+    if (start > end) return null
+    if (start == end) return start.takeIf { mountainArr.get(it) == target }
+    val midIndex = (start + end) / 2
+    val midValue = mountainArr.get(midIndex)
+    if (midValue == target) return midIndex
+
+    return if (desc xor (midValue < target))
+      findNumber(mountainArr, midIndex + 1, end, target, desc)
+    else
+      findNumber(mountainArr, start, midIndex - 1, target, desc)
+  }
+
+  fun findTop(mountainArr: MountainArray, start: Int, end: Int): Int {
+    if (start == end) return start
+    val a = (start + end) / 2
+    val b = a + 1
+
+    if (mountainArr.get(a) > mountainArr.get(b)) {
+      return findTop(mountainArr, start, a)
     }
 
-    return result.size
+    return findTop(mountainArr, b, end)
   }
 }
--- leetcode-kotlin/src/test/kotlin/com/github/anddd7/leetcode/SolutionTest.kt
+++ leetcode-kotlin/src/test/kotlin/com/github/anddd7/leetcode/SolutionTest.kt
@@ -5,7 +5,15 @@ import org.junit.Test
 class SolutionTest {
   @Test
   fun test() {
-    val printKMoves = Solution.subarrayBitwiseORs(intArrayOf(13, 4, 2))
+    val listOf = listOf(0, 1, 2, 4, 2, 1)
+
+    val printKMoves = Solution.findInMountainArray(
+        3,
+        object : Solution.MountainArray {
+          override fun get(index: Int) = listOf[index]
+          override fun length() = listOf.size
+        }
+    )
     println(printKMoves)
   }
 }
