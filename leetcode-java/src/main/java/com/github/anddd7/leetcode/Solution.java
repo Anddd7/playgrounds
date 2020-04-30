02f6b56d9906de7d993e233c35fb7acfa5aeeb50
Anddd7 <liaoad_space@sina.com>
2020-04-30 17:24:53 +0800

    [leetcode-1003] https://leetcode-cn.com/problems/check-if-word-is-valid-after-substitutions/submissions/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -1,31 +1,26 @@
 package com.github.anddd7.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public boolean isHappy(int n) {
-    int fast = n;
-    int slow = n;
-    while (fast != 1) {
-      slow = nextNumber(slow);
-      fast = nextNumber(nextNumber(fast));
-
-      if (slow == fast && fast != 1) {
-        return false;
+  /**
+   * 类似()匹配
+   */
+  public boolean isValid(String S) {
+    Deque<Character> stack = new ArrayDeque<>(S.length());
+    for (char c : S.toCharArray()) {
+      if (c == 'c') {
+        if (stack.size() < 2 || stack.pop() != 'b' || stack.pop() != 'a') {
+          return false;
+        }
+      } else {
+        stack.push(c);
       }
     }
-
-    return true;
-  }
-
-  private int nextNumber(int n) {
-    int sum = 0;
-    while (n > 0) {
-      int d = n % 10;
-      n = n / 10;
-      sum += d * d;
-    }
-    return sum;
+    return stack.isEmpty();
   }
 }
