eef39d99f69dc2c11664faa37763737f5ea000b8
Anddd7 <liaoad_space@sina.com>
2020-05-07 10:40:50 +0800

    [leetcode-572] https://leetcode-cn.com/problems/subtree-of-another-tree/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -4,34 +4,37 @@ public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  /**
-   * a^n = a^(10x+y) = a^10x * a^y
-   * <p>将n分隔为多位数
-   * a * b % x = (a % x) * (b % x) % x
-   * <p>逐步取模, 防止溢出
-   */
-  public int superPow(int a, int[] b) {
-    return superPow(a % 1337, b, b.length - 1);
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
   }
 
-  private int superPow(int a, int[] b, int index) {
-    if (index < 0) {
-      return 1;
+  public boolean isSubtree(TreeNode s, TreeNode t) {
+    if (t == null) {
+      return true;
     }
-
-    int mod = b[index];
-    int current = myPow(a, mod);
-    int previous = myPow(superPow(a, b, index - 1), 10);
-
-    return (current * previous) % 1337;
+    if (s == null) {
+      return false;
+    }
+    return isSameTree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
   }
 
-  private int myPow(int a, int b) {
-    int result = 1;
-    for (int i = 0; i < b; i++) {
-      result *= a;
-      result %= 1337;
+  public boolean isSameTree(TreeNode s, TreeNode t) {
+    if (s == null && t == null) {
+      return true;
     }
-    return result;
+    if (s == null || t == null) {
+      return false;
+    }
+    if (s.val != t.val) {
+      return false;
+    }
+    return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
   }
 }
