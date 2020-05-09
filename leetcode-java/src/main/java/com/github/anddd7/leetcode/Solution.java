1077f5ed0e29c6a43a9b15055ade335e6f9e3275
Anddd7 <liaoad_space@sina.com>
2020-05-09 20:08:13 +0800

    [leetcode-900] https://leetcode-cn.com/problems/rle-iterator/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -4,37 +4,33 @@ public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public int mySqrt(int x) {
-    int start = 0, end = x;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      long value = (long) mid * mid;
-      if (value == x) {
-        return mid;
+  class RLEIterator {
+
+    private int[] flags;
+    private int indexOfTime = 0;
+    private int indexOfNumber = 1;
+
+    public RLEIterator(int[] A) {
+      flags = A;
+    }
+
+    private boolean isEmpty() {
+      return indexOfTime == flags.length;
+    }
+
+    public int next(int n) {
+      while (n > 0 && !isEmpty()) {
+        int time = flags[indexOfTime];
+        if (n > time) {
+          n -= time;
+          indexOfTime += 2;
+          indexOfNumber += 2;
+        } else {
+          flags[indexOfTime] = time - n;
+          return flags[indexOfNumber];
+        }
       }
-      if (value < x) {
-        start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
+      return -1;
     }
-    return end;
-  }
-
-//  public int mySqrt(int x) {
-//    return findMySqrt(x, 0, x);
-//  }
-
-  private int findMySqrt(int x, long start, long end) {
-    if (start >= end) {
-      return (int) start;
-    }
-
-    long mid = (start + end + 1) / 2;
-    long value = mid * mid;
-    if (value <= x) {
-      return findMySqrt(x, mid, end);
-    }
-    return findMySqrt(x, start, mid - 1);
   }
 }
