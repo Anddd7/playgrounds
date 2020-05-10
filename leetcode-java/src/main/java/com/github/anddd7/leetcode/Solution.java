8eb613208900bd45c7feb7570f84b5773beff436
Anddd7 <liaoad_space@sina.com>
2020-05-10 16:57:11 +0800

    [leetcode-236] https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -4,19 +4,29 @@ public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public int findLength(int[] A, int[] B) {
-    int[][] dp = new int[A.length + 1][B.length + 1];
-    int max = 0;
+  public class TreeNode {
 
-    for (int i = A.length - 1; i >= 0; i--) {
-      for (int j = B.length - 1; j >= 0; j--) {
-        if (A[i] == B[j]) {
-          dp[i][j] = dp[i + 1][j + 1] + 1;
-          max = Math.max(max, dp[i][j]);
-        }
-      }
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
     }
+  }
 
-    return max;
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || root == p || root == q) {
+      return root;
+    }
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+    if (left == null) {
+      return right;
+    }
+    if (right == null) {
+      return left;
+    }
+    return root;
   }
 }
