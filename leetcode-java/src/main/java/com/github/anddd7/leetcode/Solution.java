3624fe5d24eff104fdfde19d8783a43f92aa8fc2
Anddd7 <liaoad_space@sina.com>
2020-05-14 13:47:07 +0800

    [leetcode-面试05.03] https://leetcode-cn.com/problems/reverse-bits-lcci/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -4,11 +4,24 @@ public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public int singleNumber(int[] nums) {
-    int result = 0;
-    for (int num : nums) {
-      result ^= num;
+  public int reverseBits(int num) {
+    if (num == 0) {
+      return 1;
     }
-    return result;
+
+    int pre = 0, cur = 0;
+    int max = 0;
+
+    while (num > 0) {
+      if ((1 & num) == 1) {
+        cur++;
+      } else {
+        max = Math.max(max, pre + cur + 1);
+        pre = cur;
+        cur = 0;
+      }
+      num >>= 1;
+    }
+    return Math.max(max, pre + cur + 1);
   }
 }
