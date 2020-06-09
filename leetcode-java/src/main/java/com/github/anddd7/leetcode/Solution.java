94bdcad27d29453033583f69cf09c77d21df7f6a
Anddd7 <liaoad_space@sina.com>
2020-06-09 13:06:12 +0800

    [leetcode-面试46] https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -1,53 +1,34 @@
 package com.github.anddd7.leetcode;
 
-import java.util.Arrays;
-
 public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  int[] state;
+  /**
+   * 动态规划 [1 2 2 5 8 ]
+   * <p>fi-2 fi-1 fi, 表示0-i的翻译结果种类数</p>
+   * <p>1: 只能有一个可能的翻译结果 a -> 0+1=1</p>
+   * <p>2 -> 1+1=2</p>
+   * <p>2 -> 1+2=3</p>
+   * <p>5 -> 2+3=5</p>
+   * <p>8: 58不合法, 之前的fi-2不会再叠加 -> (3)+5=5</p>
+   */
+  public int translateNum(int num) {
+    String s = String.valueOf(num);
+    // i = 0
+    int pre2 = 0;
+    int pre1 = 1;
+    int total = 1;
+    for (int i = 1; i < s.toCharArray().length; i++) {
+      pre2 = pre1;
+      pre1 = total;
+      total = pre1;
 
-  public boolean equationsPossible(String[] equations) {
-    state = new int[26];
-    for (int i = 0; i < state.length; i++) {
-      state[i] = i;
-    }
-
-    for (String equation : equations) {
-      int x = equation.charAt(0) - 'a';
-      int y = equation.charAt(3) - 'a';
-      boolean isEquals = equation.charAt(1) == '=';
-
-      if (isEquals) {
-        union(x, y);
+      boolean isValid = s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) < '6');
+      if (isValid) {
+        total += pre2;
       }
     }
-
-    System.out.println(Arrays.toString(state));
-
-    for (String equation : equations) {
-      int x = equation.charAt(0) - 'a';
-      int y = equation.charAt(3) - 'a';
-      boolean isEquals = equation.charAt(1) == '=';
-
-      if (!isEquals && find(x) == find(y)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  private void union(int x, int y) {
-    state[find(x)] = find(y);
-  }
-
-  private int find(int i) {
-    if (state[i] == i) {
-      return i;
-    }
-    int parent = find(state[i]);
-    state[i] = parent;
-    return parent;
+    return total;
   }
 }
