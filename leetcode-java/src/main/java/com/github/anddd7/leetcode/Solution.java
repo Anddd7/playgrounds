f1c24b919a517ab9f0932f42feeb4c4c85b7fffc
Anddd7 <liaoad_space@sina.com>
2020-06-10 10:05:20 +0800

    [leetcode-9] https://leetcode-cn.com/problems/palindrome-number

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -5,30 +5,23 @@ public class Solution {
   public static final Solution INSTANCE = new Solution();
 
   /**
-   * 动态规划 [1 2 2 5 8 ]
-   * <p>fi-2 fi-1 fi, 表示0-i的翻译结果种类数</p>
-   * <p>1: 只能有一个可能的翻译结果 a -> 0+1=1</p>
-   * <p>2 -> 1+1=2</p>
-   * <p>2 -> 1+2=3</p>
-   * <p>5 -> 2+3=5</p>
-   * <p>8: 58不合法, 之前的fi-2不会再叠加 -> (3)+5=5</p>
+   * 翻转一般的数字, 比较
+   * 121-> 12 vs 1 -> 1 vs 12
+   * 1221 -> 122 vs 1 -> 12 vs 12
    */
-  public int translateNum(int num) {
-    String s = String.valueOf(num);
-    // i = 0
-    int pre2 = 0;
-    int pre1 = 1;
-    int total = 1;
-    for (int i = 1; i < s.toCharArray().length; i++) {
-      pre2 = pre1;
-      pre1 = total;
-      total = pre1;
-
-      boolean isValid = s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) < '6');
-      if (isValid) {
-        total += pre2;
-      }
+  public boolean isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+      return false;
     }
-    return total;
+    if (x < 10) {
+      return true;
+    }
+
+    int reverse = 0;
+    while (x > reverse) {
+      reverse = reverse * 10 + x % 10;
+      x = x / 10;
+    }
+    return x == reverse || x == reverse / 10;
   }
 }
