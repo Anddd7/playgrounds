869828878d0be3ce03b76f2e2d0d8cf2e3c12ab7
Anddd7 <liaoad_space@sina.com>
2020-06-16 10:29:30 +0800

    [leetcode-297] https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -1,36 +1,77 @@
 package com.github.anddd7.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public String longestCommonPrefix(String[] strs) {
-    if (strs.length == 0) {
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public String serialize(TreeNode root) {
+    if (root == null) {
       return "";
     }
-    if (strs.length == 1) {
-      return strs[0];
+    List<String> result = new ArrayList<>();
+    serialize(root, result);
+    return String.join(",", result);
+  }
+
+  private void serialize(TreeNode node, List<String> storage) {
+    if (node != null) {
+      storage.add(String.valueOf(node.val));
+      serialize(node.left, storage);
+      serialize(node.right, storage);
+    } else {
+      storage.add("None");
     }
-    String min = strs[0];
-    for (String str : strs) {
-      if (str.length() == 0) {
-        return "";
-      }
-      if (str.length() < min.length()) {
-        min = str;
-      }
+  }
+
+  public TreeNode deserialize(String data) {
+    if (data.isEmpty()) {
+      return null;
+    }
+    String[] strs = data.split(",");
+    TreeNode root = new TreeNode(Integer.parseInt(strs[0]));
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    stack.push(root);
+    deserialize(root, strs, 1);
+    return root;
+  }
+
+  private int deserialize(TreeNode current, String[] strs, int pos) {
+    int leftIndex = pos;
+    String left = strs[leftIndex];
+    int endLeft;
+    if ("None".equals(left)) {
+      endLeft = leftIndex;
+    } else {
+      current.left = new TreeNode(Integer.parseInt(left));
+      endLeft = deserialize(current.left, strs, leftIndex + 1);
     }
 
-    int index = 0;
-    while (index < min.length()) {
-      char current = min.charAt(index);
-      for (String str : strs) {
-        if (str.charAt(index) != current) {
-          return min.substring(0, index);
-        }
-      }
-      index++;
+    int rightIndex = endLeft + 1;
+    String right = strs[rightIndex];
+    int endRight;
+    if ("None".equals(right)) {
+      endRight = rightIndex;
+    } else {
+      current.right = new TreeNode(Integer.parseInt(right));
+      endRight = deserialize(current.right, strs, rightIndex + 1);
     }
-    return min;
+
+    return endRight;
   }
 }
--- leetcode-java/src/test/java/com/github/anddd7/leetcode/SolutionTest.java
+++ leetcode-java/src/test/java/com/github/anddd7/leetcode/SolutionTest.java
@@ -1,10 +1,15 @@
 package com.github.anddd7.leetcode;
 
+import com.github.anddd7.leetcode.Solution.TreeNode;
+
 class SolutionTest {
 
   public static void main(String[] args) {
+    TreeNode root = Solution.INSTANCE.deserialize(
+        "1,2,None,None,3,4,None,None,5,None,None"
+    );
     System.out.println(
-        Solution.INSTANCE.pickIndex()
+        Solution.INSTANCE.serialize(root)
     );
   }
 }
