7cf2f49533aa2be54897d1a73159bdfc459b6fb7
Anddd7 <liaoad_space@sina.com>
2020-06-16 09:30:23 +0800

    [leetcode-14] https://leetcode-cn.com/problems/longest-common-prefix/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -1,49 +1,36 @@
 package com.github.anddd7.leetcode;
 
-import java.util.Random;
-
 public class Solution {
 
-  public static final Solution INSTANCE = new Solution(new int[]{1, 3});
+  public static final Solution INSTANCE = new Solution();
 
-
-  /**
-   * 前缀和: 想象w[i]是一根线段, 前缀和就是把它们连接起来的一条更长的线段 当在线段上随机取一个点(数字), 它落在w[i]的几率和w[i]的长度成正比
-   */
-  private final int[] psum;
-  private final Random r = new Random();
-
-  public Solution(int[] w) {
-    psum = new int[w.length];
-    int tot = 0;
-    for (int i = 0; i < w.length; i++) {
-      tot += w[i];
-      psum[i] = tot;
+  public String longestCommonPrefix(String[] strs) {
+    if (strs.length == 0) {
+      return "";
     }
-  }
-
-  public int pickIndex() {
-    int target = r.nextInt(psum[psum.length - 1]);
-    return find(psum, 0, psum.length - 1, target);
-  }
-
-  /**
-   * 二分法找最小的index, 使target<psum[index]
-   */
-  private int find(int[] psum, int s, int e, int target) {
-    if (s == e) {
-      return s;
+    if (strs.length == 1) {
+      return strs[0];
+    }
+    String min = strs[0];
+    for (String str : strs) {
+      if (str.length() == 0) {
+        return "";
+      }
+      if (str.length() < min.length()) {
+        min = str;
+      }
     }
 
-    int mid = (s + e) / 2;
-    int value = psum[mid];
-
-    if (target < value) {
-      return find(psum, s, mid, target);
+    int index = 0;
+    while (index < min.length()) {
+      char current = min.charAt(index);
+      for (String str : strs) {
+        if (str.charAt(index) != current) {
+          return min.substring(0, index);
+        }
+      }
+      index++;
     }
-    if (target > value) {
-      return find(psum, mid + 1, e, target);
-    }
-    return mid + 1;
+    return min;
   }
 }
