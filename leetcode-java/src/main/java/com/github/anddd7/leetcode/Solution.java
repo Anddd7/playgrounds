7b2b03c48cb22af9e5f07319801b3eb06d331fae
Anddd7 <liaoad_space@sina.com>
2020-06-19 10:33:48 +0800

    [leetcode-125] https://leetcode-cn.com/problems/valid-palindrome/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -1,50 +1,29 @@
 package com.github.anddd7.leetcode;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-      val = x;
+  public boolean isPalindrome(String s) {
+    if (s.isEmpty()) {
+      return true;
     }
-  }
+    int x = 0, y = s.length() - 1;
+    while (x < y) {
+      int front = s.codePointAt(x);
+      int end = s.codePointAt(y);
 
-  public TreeNode recoverFromPreorder(String S) {
-    Deque<TreeNode> stack = new LinkedList<>();
-    int i = 0;
-    while (i < S.length()) {
-      int deep = 0;
-      while (S.charAt(i) == '-') {
-        ++deep;
-        ++i;
-      }
-      int value = 0;
-      while (i < S.length() && Character.isDigit(S.charAt(i))) {
-        value = value * 10 + (S.charAt(i) - '0');
-        ++i;
-      }
-      TreeNode node = new TreeNode(value);
-      if (deep == stack.size()) {
-        if (!stack.isEmpty()) {
-          stack.peek().left = node;
-        }
+      if (!Character.isLetterOrDigit(front)) {
+        x++;
+      } else if (!Character.isLetterOrDigit(end)) {
+        y--;
+      } else if (Character.toLowerCase(front) == Character.toLowerCase(end)) {
+        x++;
+        y--;
       } else {
-        while (deep != stack.size()) {
-          stack.pop();
-        }
-        stack.peek().right = node;
+        return false;
       }
-      stack.push(node);
     }
-    return stack.peekLast();
+    return true;
   }
 }
--- leetcode-java/src/test/java/com/github/anddd7/leetcode/SolutionTest.java
+++ leetcode-java/src/test/java/com/github/anddd7/leetcode/SolutionTest.java
@@ -1,15 +1,12 @@
 package com.github.anddd7.leetcode;
 
-import com.github.anddd7.leetcode.Solution.TreeNode;
-
 class SolutionTest {
 
   public static void main(String[] args) {
-    TreeNode root = Solution.INSTANCE.recoverFromPreorder(
-        "1-401--349---90--88"
-    );
     System.out.println(
-        root
+        Solution.INSTANCE.isPalindrome(
+            "A man, a plan, a canal: Panama"
+        )
     );
   }
 }
