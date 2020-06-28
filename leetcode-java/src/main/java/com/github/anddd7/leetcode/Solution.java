23a0d1bb122ca27da404489c8e0639d595c0de11
Anddd7 <liaoad_space@sina.com>
2020-06-28 10:05:53 +0800

    [leetcode-209] https://leetcode-cn.com/problems/minimum-size-subarray-sum/

--- leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
+++ leetcode-java/src/main/java/com/github/anddd7/leetcode/Solution.java
@@ -1,42 +1,26 @@
 package com.github.anddd7.leetcode;
 
-import java.util.Arrays;
-
 public class Solution {
 
   public static final Solution INSTANCE = new Solution();
 
-  public int threeSumClosest(int[] nums, int target) {
-    int result = nums[0] + nums[1] + nums[2];
-    Arrays.sort(nums);
+  public int minSubArrayLen(int s, int[] nums) {
+    int start = 0, end = 0;
+    int current = 0;
+    int minLength = nums.length + 1;
 
-    for (int i = 0; i < nums.length - 2; i++) {
-      int a = nums[i];
-      if (i != 0 && nums[i] == nums[i - 1]) {
-        continue;
-      }
-      int s = i + 1, e = nums.length - 1;
-      while (s < e) {
-        int b = nums[s];
-        int c = nums[e];
-        int value = a + b + c;
-
-        if (value == target) {
-          return value;
-        }
-
-        if (value < target) {
-          s++;
-        } else {
-          e--;
-        }
-
-        if (Math.abs(value - target) < Math.abs(result - target)) {
-          result = value;
-        }
+    while (end < nums.length) {
+      int total = current + nums[end];
+      int length = end + 1 - start;
+      if (total >= s) {
+        minLength = Math.min(minLength, length);
+        current = current - nums[start++];
+      } else {
+        current = total;
+        end++;
       }
     }
 
-    return result;
+    return minLength > nums.length ? 0 : minLength;
   }
 }
--- leetcode-java/src/test/java/com/github/anddd7/leetcode/SolutionTest.java
+++ leetcode-java/src/test/java/com/github/anddd7/leetcode/SolutionTest.java
@@ -4,8 +4,9 @@ class SolutionTest {
 
   public static void main(String[] args) {
     System.out.println(
-        Solution.INSTANCE.threeSumClosest(
-            new int[]{-3, -2, -5, 3, -4}, -1
+        Solution.INSTANCE.minSubArrayLen(
+            7,
+            new int[]{2, 3, 1, 2, 4, 3}
         )
     );
   }
