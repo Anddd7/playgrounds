0e8b20df797bcb1cbee8c6e838ca9798a64cbe90
Anddd7 <liaoad_space@sina.com>
2020-07-07 11:54:47 +0800

    [leetcode-112] https://leetcode-cn.com/problems/path-sum/

--- golang-quick-start/leetcode/solution_test.go
+++ golang-quick-start/leetcode/solution_test.go
@@ -22,40 +22,37 @@ func Test_uniquePathsWithObstacles(t *testing.T) {
 }
 
 func Test_minDeletionSize(t *testing.T) {
-	inputs := []string{
-		"vqyoysnpxbjiitandmvugsqpfmggkv",
-		"uzdfeclxepjzfecmsxrqqkcomtrnvm",
-		"yvhwrsapfffwehdmvqwxstgeexfeua",
-		"awjymwysjpazpgdeqtvdiebfwuapin",
-		"odhihlbvsnximvdwqntdeqptigiyik",
-		"qtrfpwiilxskcieilfvarqbnpdxham",
-		"whvrqkdwuzbcaagsmlfvfbeataygud",
-		"kncwqrmejjmhtfhppsrdmzqperwlww",
-		"hgphuwaumjjibzhvvejpniopjxizie",
-		"bxvccswqevnudqicgrvjecfqpeppob",
-		"nnmvncnpbksdjyjjelsjizliicxpgz",
-		"oifmofrkbgpxlhkcbibwaoiygmqqio",
-		"ekdfyvsumngcfjlydgpmhgjjyfovfi",
-		"fyqryrpkvauhkylmfzhuasjxpqrohx",
-		"rdvjglvpavzdmtobnpjfwdwivhrpsj",
-		"zahrkuiejecndfprwysunznialtfok",
-		"jlrgpfdptlolmlqoophhciiqjnxdkh",
-		"bhbsdukebqvvemrcunboipprcbrfcl",
-		"kreyeyvsmufolvsrzdyeqpuqlieeij",
-		"vgosaxsfnbsndstjohgyknyionhoga",
-		"igmnlibpadandgtugbgxpxwlqbknmv",
-		"mjdbxxprxbjegvtthlrenhfpdlamww",
-		"qfssehellhvqyntozbrizixptppfpr",
-		"utghfndlcturahtcvmqrjyxqfhrsxt",
-		"xvminqhybbiadetniqfwubqxmjokjv",
-		"udfckncwvhcrmxtbkqbqqptymlqnss",
-		"gwwcmterazvyakuvwtyhthfiohlywq",
-		"mpieryurvarojfvhfbbcwepdeoedri",
-		"lpaonsugmlzuweyvrrlgwwdjsgwmoh",
-		"kexyawgkinwvjvzwvofqlthmhaicgs",
-	}
+	inputs := []string{"vqyoysnpxbjiitandmvugsqpfmggkv", "uzdfeclxepjzfecmsxrqqkcomtrnvm", "yvhwrsapfffwehdmvqwxstgeexfeua", "awjymwysjpazpgdeqtvdiebfwuapin", "odhihlbvsnximvdwqntdeqptigiyik", "qtrfpwiilxskcieilfvarqbnpdxham", "whvrqkdwuzbcaagsmlfvfbeataygud", "kncwqrmejjmhtfhppsrdmzqperwlww", "hgphuwaumjjibzhvvejpniopjxizie", "bxvccswqevnudqicgrvjecfqpeppob", "nnmvncnpbksdjyjjelsjizliicxpgz", "oifmofrkbgpxlhkcbibwaoiygmqqio", "ekdfyvsumngcfjlydgpmhgjjyfovfi", "fyqryrpkvauhkylmfzhuasjxpqrohx", "rdvjglvpavzdmtobnpjfwdwivhrpsj", "zahrkuiejecndfprwysunznialtfok", "jlrgpfdptlolmlqoophhciiqjnxdkh", "bhbsdukebqvvemrcunboipprcbrfcl", "kreyeyvsmufolvsrzdyeqpuqlieeij", "vgosaxsfnbsndstjohgyknyionhoga", "igmnlibpadandgtugbgxpxwlqbknmv", "mjdbxxprxbjegvtthlrenhfpdlamww", "qfssehellhvqyntozbrizixptppfpr", "utghfndlcturahtcvmqrjyxqfhrsxt", "xvminqhybbiadetniqfwubqxmjokjv", "udfckncwvhcrmxtbkqbqqptymlqnss", "gwwcmterazvyakuvwtyhthfiohlywq", "mpieryurvarojfvhfbbcwepdeoedri", "lpaonsugmlzuweyvrrlgwwdjsgwmoh", "kexyawgkinwvjvzwvofqlthmhaicgs"}
 
 	result := minDeletionSize(inputs)
+
+	fmt.Println(result)
+}
+
+func Test_hasPathSum(t *testing.T) {
+	// inputs := &TreeNode{
+	// 	5,
+	// 	&TreeNode{
+	// 		4,
+	// 		&TreeNode{11,
+	// 			&TreeNode{7, nil, nil},
+	// 			&TreeNode{2, nil, nil},
+	// 		},
+	// 		nil,
+	// 	},
+	// 	&TreeNode{
+	// 		8,
+	// 		&TreeNode{13, nil, nil},
+	// 		&TreeNode{4, nil, &TreeNode{1, nil, nil}},
+	// 	},
+	// }
+	inputs := &TreeNode{
+		-2,
+		nil,
+		&TreeNode{-3, nil, nil},
+	}
+
+	result := hasPathSum(inputs, -2)
 
 	fmt.Println(result)
 }
